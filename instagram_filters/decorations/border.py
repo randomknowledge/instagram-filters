b0ddc6419e96f54003ded34d8a5831ae74f95a33
Florian Finke <flo@randomknowledge.org>
2015-06-03 12:17:48 +0200

    * make it work with pillow

--- instagram_filters/decorations/border.py
+++ instagram_filters/decorations/border.py
@@ -1,9 +1,10 @@
 from instagram_filters.filter import Filter
 
+
 class Border(Filter):
-	
-	def border(self, color = 'black', width = 20):
-		self.execute("convert {filename} -bordercolor {color} -border {bwidth}x{bwidth} {filename}",
-			color = color,
-			bwidth = width
-		)
+    def border(self, color='black', width=20):
+        self.execute(
+            "convert {filename} -bordercolor {color} -border {bwidth}x{bwidth} {filename}",
+            color=color,
+            bwidth=width,
+        )
--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup
 
 config = {
-	'name': 'instagram-filters',
-	'version': '0.1',
-	'description': 'Instagram-like image filters',
-	'license': 'LICENSE.txt',
-	'url': 'https://github.com/acoomans/instagram-filters',
-	'author': 'Arnaud Coomans',
-	'author_email': 'arnaud.coomans@gmail.com',
-	'install_requires': ['PIL'],
-	'packages': ['instagram_filters', 'instagram_filters.filters', 'instagram_filters.decorations'],
-	'data_files' : [
-		('instagram_filters/decorations/frames', ['instagram_filters/decorations/frames/Kelvin.jpg', 'instagram_filters/decorations/frames/Nashville.jpg'])
-	],
-	'zip_safe': False
+    'name': 'instagram-filters',
+    'version': '0.1',
+    'description': 'Instagram-like image filters',
+    'license': 'LICENSE.txt',
+    'url': 'https://github.com/acoomans/instagram-filters',
+    'author': 'Arnaud Coomans',
+    'author_email': 'arnaud.coomans@gmail.com',
+    'install_requires': ['Pillow'],
+    'packages': ['instagram_filters', 'instagram_filters.filters', 'instagram_filters.decorations'],
+    'data_files': [
+        ('instagram_filters/decorations/frames',
+         ['instagram_filters/decorations/frames/Kelvin.jpg', 'instagram_filters/decorations/frames/Nashville.jpg'])
+    ],
+    'zip_safe': False
 }
 
 setup(**config)
